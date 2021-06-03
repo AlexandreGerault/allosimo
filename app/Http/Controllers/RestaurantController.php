22fed526e3a312886c35e0300190318bc4bd67de
Alexandre Gérault <alexandre@hexium.io>
2021-06-03 21:51:11 +0200

    feat(restaurant): use RestaurantPolicy in controller

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -14,6 +14,7 @@ class RestaurantController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->only(['create', 'store', 'edit', 'update']);
+        $this->authorizeResource(Restaurant::class);
     }
 
     /**
