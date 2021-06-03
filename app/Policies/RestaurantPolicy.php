3784943778b4e421a3c1092c859d1ac93588f5f2
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 01:56:55 +0200

    feat(restaurant): add edition policy

--- app/Policies/RestaurantPolicy.php
+++ app/Policies/RestaurantPolicy.php
@@ -53,7 +53,7 @@ class RestaurantPolicy
      */
     public function update(User $user, Restaurant $restaurant)
     {
-        //
+        return $user->hasRole('administrateur');
     }
 
     /**
