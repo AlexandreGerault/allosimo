63546406aa3ec4e020daa3a4be90eec98bc5b3f1
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 01:01:53 +0200

    feat(restaurant): display list of restaurants

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -11,12 +11,12 @@
         <x-button-link href="{{ route('restaurant.create') }}">Ajouter un restaurant</x-button-link>
     </x-container>
     <x-dashboard-section>
+        <div class="grid grid-cols-2 gap-4">
         @forelse($restaurants as $restaurant)
-            <div>
-                {{ $restaurant->name }}
-            </div>
+                <x-restaurant-card :restaurant="$restaurant"/>
         @empty
             <p>Aucun restaurant enregistré pour le moment.</p>
         @endforelse
+        </div>
     </x-dashboard-section>
 </x-app-layout>
