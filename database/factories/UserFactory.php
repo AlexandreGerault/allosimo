32a1ab5d0b9ac7af74889c5ed2e98ca6648ad856
Alexandre Gérault <alexandre@hexium.io>
2021-06-03 18:56:38 +0200

    feat(auth): add fields to factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,40 +8,35 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name'              => $this->faker->name(),
+            'email'             => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+            'address'           => $this->faker->address,
+            'town'              => $this->faker->city,
+            'phone'             => $this->faker->phoneNumber
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
+    public function unverified(): UserFactory
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        return $this->state(
+            function (array $attributes) {
+                return [
+                    'email_verified_at' => null,
+                ];
+            }
+        );
+    }
+
+    public function role(string $role): UserFactory
+    {
+        return $this->state(['role' => $role]);
     }
 }
