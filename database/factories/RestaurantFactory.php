342fd3680f1122c114c4871bd9e8d1c14cf1e4ea
Alexandre Gérault <alexandre@hexium.io>
2021-06-03 23:56:43 +0200

    feat(restaurant): create restaurant factory

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -7,22 +7,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RestaurantFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Restaurant::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
-            //
+            'name'        => $this->faker->name,
+            'description' => $this->faker->text,
+            'town'        => $this->faker->city,
+            'stars'       => $this->faker->numberBetween(0, 10)
         ];
     }
 }
