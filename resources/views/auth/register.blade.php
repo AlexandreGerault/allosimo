1733c4e182d2310c7f4ab8f3f532e57906408578
Alexandre Gérault <alexandre@hexium.io>
2021-06-03 21:32:49 +0200

    feat(register): add new fields to view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,56 +2,78 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
             </a>
         </x-slot>
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <x-auth-validation-errors class="mb-4" :errors="$errors"/>
 
         <form method="POST" action="{{ route('register') }}">
-            @csrf
+        @csrf
 
-            <!-- Name -->
+        <!-- Name -->
             <div>
-                <x-label for="name" :value="__('Name')" />
+                <x-label for="name" value="Nom et prénom"/>
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                         autofocus/>
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" value="Adresse mail"/>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required/>
+            </div>
+
+            <!-- Phone Number -->
+            <div class="mt-4">
+                <x-label for="phone" value="Téléphone"/>
+
+                <x-input id="phone" class="block mt-1 w-full" type="tel" name="phone" :value="old('phone')" required/>
+            </div>
+
+            <!-- Town -->
+            <div class="mt-4">
+                <x-label for="town" value="Ville"/>
+
+                <x-input id="town" class="block mt-1 w-full" type="text" name="town" :value="old('town')" required/>
+            </div>
+
+            <!-- Address -->
+            <div class="mt-4">
+                <x-label for="address" value="Adresse"/>
+
+                <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required/>
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" value="Mot de passe"/>
 
                 <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
+                         type="password"
+                         name="password"
+                         required autocomplete="new-password"/>
             </div>
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-label for="password_confirmation" value="Confirmer le mot de passe"/>
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+                         type="password"
+                         name="password_confirmation" required/>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    Déjà inscrit ?
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    Inscription
                 </x-button>
             </div>
         </form>
