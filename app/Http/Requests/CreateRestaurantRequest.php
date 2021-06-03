6c55b4365802f284cb9895286cd26bec5b8ad5b4
Alexandre Gérault <alexandre@hexium.io>
2021-06-03 23:56:29 +0200

    feat(restaurant): create restaurant validator

---
+++ app/Http/Requests/CreateRestaurantRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRestaurantRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'        => ['required', 'string', 'unique:restaurants,name'],
+            'description' => ['required', 'string'],
+            'logo'        => ['required', 'image', 'max:1024'],
+            'town'        => ['required', 'string'],
+            'state'       => ['nullable', 'string', 'in_array:open,closed'],
+            'type'        => ['nullable', 'string', 'in_array:restaurant,bakery'],
+            'stars'       => ['required', 'numeric', 'min:0', 'max:10']
+        ];
+    }
+}
