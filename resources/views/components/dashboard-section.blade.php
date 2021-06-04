d2c55ccf9912972395fe8cc462297284616fdcd1
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 05:48:36 +0200

    feat(ui): add heading slot for dashboard section

--- resources/views/components/dashboard-section.blade.php
+++ resources/views/components/dashboard-section.blade.php
@@ -1,9 +1,12 @@
-<div class="py-12">
+<section class="py-12">
     <x-container>
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
+                @isset($sectionHeading)
+                <h3 class="my-2 text-3xl font-semibold">{{ $sectionHeading }}</h3>
+                @endisset
                 {{ $slot }}
             </div>
         </div>
     </x-container>
-</div>
+</section>
