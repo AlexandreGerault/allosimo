b4c418f795f284d13cdae546ff5de176110ba5a7
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 21:00:01 +0200

    feat(options-category): create controller

---
+++ app/Http/Controllers/Admin/OptionCategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\OptionCategoryRequest;
+use App\Models\OptionCategory;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class OptionCategoryController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(OptionCategoryRequest $request, Restaurant $restaurant)
+    {
+    }
+
+    public function show(OptionCategory $optionCategory)
+    {
+    }
+
+    public function edit(OptionCategory $optionCategory)
+    {
+    }
+
+    public function update(OptionCategoryRequest $request, OptionCategory $optionCategory)
+    {
+    }
+
+    public function destroy(OptionCategory $optionCategory)
+    {
+    }
+}
