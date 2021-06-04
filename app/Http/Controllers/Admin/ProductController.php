75ed5914c77b6aa2736a2b5ac4c07618437b7b04
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 14:08:30 +0200

    chore(products): clean comments

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\ProductCategory;
+use App\Models\Product;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-class ProductCategoryController extends Controller
+class ProductController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(ProductCategory::class);
+        $this->authorizeResource(Product::class);
     }
 
     public function index()
@@ -21,62 +21,26 @@ class ProductCategoryController extends Controller
 
     public function create(Restaurant $restaurant): View
     {
-        return view('admin.restaurant.category.create', compact('restaurant'));
+        return view('admin.restaurant.products.create', compact('restaurant'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+    }
+    
+    public function show(Product $productCategory)
+    {
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ProductCategory  $productCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ProductCategory $productCategory)
+    public function edit(Product $productCategory)
     {
-        //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ProductCategory  $productCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ProductCategory $productCategory)
+    public function update(Request $request, Product $productCategory)
     {
-        //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\ProductCategory  $productCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, ProductCategory $productCategory)
+    public function destroy(Product $productCategory)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ProductCategory  $productCategory
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(ProductCategory $productCategory)
-    {
-        //
     }
 }
