d3d60a78feff548573d8f4a64e42d445a2d29c00
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 13:31:48 +0200

    feat(category): update use ProductCategoryController namespace

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\RestaurantController;
-use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\Admin\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
