ea485aa0b12d7391ebbd251d932ba44db7d764c0
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 14:15:26 +0200

    feat(restaurant): add back navigation link

--- resources/views/admin/restaurant/show.blade.php
+++ resources/views/admin/restaurant/show.blade.php
@@ -3,13 +3,22 @@
         Restaurant {{ $restaurant->name }}
     </x-slot>
 
+    <x-container class="mt-12">
+        <div class="flex justify-center">
+            <x-button-link href="{{ route('admin.restaurant.index') }}">
+                Revenir à la liste des restaurants
+            </x-button-link>
+        </div>
+    </x-container>
+
     <x-dashboard-section>
         <x-slot name="sectionHeading">
             Informations générales
         </x-slot>
 
         <div class="flex gap-4">
-            <img src="{{ Storage::url('restaurants/' . $restaurant->name . '.png') }}" alt="Not loaded" class="block w-24 h-24"/>
+            <img src="{{ Storage::url('restaurants/' . $restaurant->name . '.png') }}" alt="Not loaded"
+                 class="block w-24 h-24"/>
             <div class="mt-4">
                 <p class="text-gray-900 font-semibold">{{ $restaurant->name }}</p>
                 <p class="text-gray-800">{{ $restaurant->description }}</p>
@@ -19,15 +28,15 @@
 
     <x-dashboard-section>
         <x-slot name="sectionHeading">
-            Catégories
+            Produits
         </x-slot>
 
-        <x-button-link href="{{ route('admin.restaurant.category.create', $restaurant) }}">
-            Ajouter une catégorie
+        <x-button-link href="{{ route('admin.restaurant.product.create', $restaurant) }}">
+            Ajouter un nouveau produit
         </x-button-link>
 
         <div class="mt-4">
-            @forelse($restaurant->productCategories as $category)
+            @forelse($restaurant->products as $product)
             @empty
                 <p>Aucune catégorie pour le moment</p>
             @endforelse
