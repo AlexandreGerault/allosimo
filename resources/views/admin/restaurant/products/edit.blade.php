5567f17f7fcefc18e11f57b6e59cc248c67b6193
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 17:03:52 +0200

    feat(product): edit product view

---
+++ resources/views/admin/restaurant/products/edit.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        Modifier {{ $product->name }}
+    </x-slot>
+
+    <x-container class="mt-12">
+        <div class="flex justify-center">
+            <x-button-link href="{{ route('admin.restaurant.show', $restaurant) }}">
+                Revenir à la fiche du restaurant
+            </x-button-link>
+        </div>
+    </x-container>
+
+    <x-dashboard-section>
+        <x-slot name="sectionHeading">
+            Modifier {{ $product->name }}
+        </x-slot>
+
+        <x-product-form :action="route('admin.restaurant.product.update', [$restaurant, $product])" method="PUT" :product="$product"
+                        submit="Modifier"/>
+    </x-dashboard-section>
+</x-app-layout>
