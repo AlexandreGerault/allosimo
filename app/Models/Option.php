3959e29311c2a43e15591db32f11b512d80320a2
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 23:21:55 +0200

    feat(options): add category relationship

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Option extends Model
 {
     use HasFactory;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(OptionCategory::class, 'option_category_id');
+    }
 }
