498b5efefcceb6cc2a7d966ba1255622edc781e7
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 14:26:56 +0200

    chore(category): remove useless comments

--- database/migrations/2021_06_04_015214_create_product_categories_table.php
+++ database/migrations/2021_06_04_015214_create_product_categories_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateProductCategoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('product_categories', function (Blueprint $table) {
@@ -19,12 +14,7 @@ class CreateProductCategoriesTable extends Migration
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
         Schema::dropIfExists('product_categories');
