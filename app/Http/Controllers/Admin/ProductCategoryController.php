d66ccac0c619cef81f77b97a86f54bd4b68bbc8f
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 17:23:20 +0200

    feat(category): generate category controller
    Auto stash before rebase of "develop"

---
+++ app/Http/Controllers/Admin/ProductCategoryController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(ProductCategory::class);
+    }
+
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(ProductCategory $productCategory)
+    {
+    }
+
+    public function edit(ProductCategory $productCategory)
+    {
+    }
+
+    public function update(Request $request, ProductCategory $productCategory)
+    {
+    }
+
+    public function destroy(ProductCategory $productCategory)
+    {
+    }
+}
