23244df167f56bd3791b5f692fc7d20b9c5168df
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 16:49:22 +0200

    feat(product): store product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -24,8 +26,13 @@ class ProductController extends Controller
         return view('admin.restaurant.products.create', compact('restaurant'));
     }
 
-    public function store(Request $request)
+    public function store(Restaurant $restaurant, ProductRequest $request)
     {
+        $product = Product::make($request->validated());
+        $product->category()->associate(ProductCategory::query()->findOrFail($request->get('category')));
+        $restaurant->products()->save($product);
+
+        return redirect()->route('admin.restaurant.show', $restaurant);
     }
 
     public function show(Product $productCategory)
