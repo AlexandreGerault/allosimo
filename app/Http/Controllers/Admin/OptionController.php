066531beaf093b46f1ebd3ad87e7c52ed94e1e18
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 23:42:51 +0200

    feat(options): create actions

--- app/Http/Controllers/Admin/OptionController.php
+++ app/Http/Controllers/Admin/OptionController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OptionRequest;
 use App\Models\Option;
+use App\Models\OptionCategory;
+use App\Models\Restaurant;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class OptionController extends Controller
@@ -17,12 +22,15 @@ class OptionController extends Controller
     {
     }
 
-    public function create()
+    public function create(Restaurant $restaurant, OptionCategory $option_category): View
     {
+        return view('admin.restaurant.options-category.options.create', compact('restaurant', 'option_category'));
     }
 
-    public function store(Request $request)
+    public function store(OptionRequest $request, Restaurant $restaurant, OptionCategory $option_category): RedirectResponse
     {
+        $option_category->options()->save(Option::make($request->validated()));
+        return redirect()->route('admin.restaurant.show', $restaurant);
     }
 
     public function show(Option $option)
