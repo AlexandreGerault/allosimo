a4cbfc7a0d0a876f9b2dc355e7172898e14e7c95
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 17:11:18 +0200

    feat(product): add value

--- resources/views/components/input-price.blade.php
+++ resources/views/components/input-price.blade.php
@@ -6,7 +6,7 @@
         {{ $currency }}
       </span>
         </div>
-        <x-input type="number" name="{{ $name }}" id="{{ $id }}" placeholder="0" {{ $attributes }} />
+        <x-input type="number" name="{{ $name }}" id="{{ $id }}" placeholder="0" {{ $attributes }} value="{{ $value }}" />
         <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
       <span class="text-gray-500 sm:text-sm mr-6" id="price-currency">
         {{ $currencyText }}
