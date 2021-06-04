fbe636813787573349484986ef1ecf812c87fd36
Alexandre Gérault <alexandre@hexium.io>
2021-06-04 21:57:33 +0200

    feat(option-category): add default restaurant relation

--- database/factories/OptionCategoryFactory.php
+++ database/factories/OptionCategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\OptionCategory;
+use App\Models\Restaurant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OptionCategoryFactory extends Factory
@@ -12,7 +13,8 @@ class OptionCategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name
+            'name' => $this->faker->name,
+            'restaurant_id' => Restaurant::factory()->create()->id
         ];
     }
 }
