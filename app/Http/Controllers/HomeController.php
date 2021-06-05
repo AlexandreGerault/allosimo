7cd52c0b55f0ac60aa0f80ab16b8effe7409a565
Alexandre Gérault <alexandre@hexium.io>
2021-06-05 17:53:07 +0200

    feat(homepage): limit and order latest restaurant for Mahommédia

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     public function __invoke(): View
     {
-        return view('home');
+        $restaurants = Restaurant::query()
+                                 ->where('type', '=', 'restaurant')
+                                 ->where('town', '=', 'Mohammédia')
+                                 ->latest()
+                                 ->limit(6)
+                                 ->get();
+        $bakeries    = Restaurant::query()
+                                 ->where('type', '=', 'bakery')
+                                 ->where('town', '=', 'Mohammédia')
+                                 ->latest()
+                                 ->limit(6)
+                                 ->get();
+
+        return view('home')->with('restaurants', $restaurants)->with('bakeries', $bakeries);
     }
 }
