e373caba348c0856d83095c097f6e62bda4205dc
Alexandre Gérault <alexandre@hexium.io>
2021-06-05 23:58:27 +0200

    feat(cart): DTO for each entry

--- app/DTOs/CartLine.php
+++ app/DTOs/CartLine.php
@@ -10,4 +10,19 @@ class CartLine
     public function __construct(protected Product $product, protected Collection $options, protected int $amount)
     {
     }
+
+    public function product(): Product
+    {
+        return $this->product;
+    }
+
+    public function options(): Collection
+    {
+        return $this->options;
+    }
+
+    public function amount(): int
+    {
+        return $this->amount;
+    }
 }
