995c0076b58cdc573e157e3ab980a7000fdd434a
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 01:51:39 +0200

    feat(order): add lines relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,13 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
 
+    const STATES = ['confirmed', 'cancelled'];
+
+    protected $fillable = ['state'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function lines(): HasMany
+    {
+        return $this->hasMany(OrderLine::class);
     }
 }
