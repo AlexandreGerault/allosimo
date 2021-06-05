4a0d9601dd7e8db964045323a9cfa6b227627563
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 01:53:09 +0200

    feat(order): test it has lines

---
+++ tests/Feature/OrderModelTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Order;
+use App\Models\OrderLine;
+use App\Models\Product;
+use App\Models\User;
+use Tests\TestCase;
+
+class OrderModelTest extends TestCase
+{
+    public function test_it_has_lines()
+    {
+        $order = Order::factory()
+                      ->for(User::factory())
+                      ->has(OrderLine::factory()->for(Product::factory())->count(3), 'lines')
+                      ->create();
+
+        $this->assertInstanceOf(OrderLine::class, $order->lines->first());
+        $this->assertCount(3, $order->lines);
+    }
+}
