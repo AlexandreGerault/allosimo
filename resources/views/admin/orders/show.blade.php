4c20659cff8f693617e241f6b2b7a5584fbde91a
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 04:10:05 +0200

    feat(order): update totals

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -24,21 +24,28 @@
         <div class="flex flex-col gap-6 mt-6">
             @foreach($order->lines as $line)
                 <div class="bg-gray-50 rounded overflow-hidden px-6 py-4">
-                    <div class="flex gap-4 justify-between">
+                    <div class="flex gap-4">
                         <div>
-                            {{ $line->product->name }}
+                            {{ $line->quantity }}
                         </div>
-                        <div>
-                            {{ $line->product->price }} DH
-                        </div>
-                    </div>
-                    <div class="pl-6">
-                        @foreach($line->options as $option)
-                            <div class="flex justify-between">
-                                <div>{{ $option->name }}</div>
-                                <div>{{ $option->price }} DH</div>
+                        <div class="flex-grow">
+                            <div class="flex gap-4 justify-between">
+                                <div>
+                                    {{ $line->product->name }}
+                                </div>
+                                <div>
+                                    {{ $line->product->price }} DH
+                                </div>
                             </div>
-                        @endforeach
+                            <div class="pl-6">
+                                @foreach($line->options as $option)
+                                    <div class="flex justify-between">
+                                        <div>{{ $option->name }}</div>
+                                        <div>{{ $option->price }} DH</div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
 
                     <div class="flex justify-end mt-2 font-semibold">
