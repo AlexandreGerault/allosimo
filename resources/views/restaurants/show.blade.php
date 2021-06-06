586b22c39cdf862d63a0071c34d68e0e30e3d41e
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 06:27:49 +0200

    feat(products): smaller image

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -35,7 +35,7 @@
                             <div class="flex flex-col gap-4">
                                 @foreach($products as $product)
                                     <div class="flex justify-between items-center">
-                                        <img src="{{ $product?->image === 'null' ? asset('img/default_product.png') : Storage::url('restaurants/'. $restaurant->name . '/products/' . $product->image) }}" alt="" class="w-24 h-24" />
+                                        <img src="{{ $product?->image === 'null' ? asset('img/default_product.png') : Storage::url('restaurants/'. $restaurant->name . '/products/' . $product->image) }}" alt="" class="w-16 h-16" />
                                         <span>{{ $product->name }}</span>
                                         <div class="flex gap-4">
                                             <span>{{ $product->price }} DH</span>
