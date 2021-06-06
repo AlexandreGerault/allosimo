fe84dea8c06aab1d4203642e2545893cb2ac7e9a
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 03:02:27 +0200

    feat(order): test admin can see orders

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\DTOs\CartLine;
 use App\Models\Option;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
 use App\Services\Cart;
@@ -33,5 +34,41 @@ class OrderTest extends TestCase
         $response->assertRedirect();
         $response->assertSessionMissing('cart');
         $this->assertDatabaseHas('orders', ['user_id' => $user->id]);
+    }
+
+    public function test_a_guest_cannot_list_orders()
+    {
+        $response = $this->get(route('admin.orders.index'));
+
+        $response->assertRedirect();
+    }
+
+    public function test_a_user_cannot_list_orders()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->get(route('admin.orders.index'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_an_admin_can_show_orders()
+    {
+        $this->actAsAdmin();
+
+        $response = $this->get(route('admin.orders.index'));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_an_admin_can_show_an_order()
+    {
+        $this->actAsAdmin();
+        $order = Order::factory()->for(User::factory())->create();
+
+        $response = $this->get(route('admin.orders.show', $order));
+
+        $response->assertSuccessful();
     }
 }
