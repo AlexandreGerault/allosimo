d41fdfd9a223bda26d21d0aea9c7ec7efddade5f
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 03:02:07 +0200

    feat(orders): admin views

---
+++ resources/views/admin/orders/index.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="title">
+        Commandes
+    </x-slot>
+
+    <x-slot name="header">
+        Commandes
+    </x-slot>
+
+    <x-dashboard-section>
+        <div class="grid sm:grid-cols-2 gap-4">
+            @forelse($orders as $order)
+                <x-order-card :order="$order" />
+            @empty
+                <p>Aucun restaurant enregistré pour le moment.</p>
+            @endforelse
+        </div>
+
+        <div class="mt-4">
+            {{ $orders->links() }}
+        </div>
+    </x-dashboard-section>
+</x-app-layout>
