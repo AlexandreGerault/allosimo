a0a76b01271d18ffe470edf81390face74a79702
Alexandre Gérault <alexandre@hexium.io>
2021-06-06 12:25:07 +0200

    feat(cart): add 1 to default quantity and justify-betweens

--- resources/views/components/product-option-card.blade.php
+++ resources/views/components/product-option-card.blade.php
@@ -2,29 +2,36 @@
 
 <x-dropdown :width="60" :auto-close="false">
     <x-slot name="trigger">
-        <x-heroicon-s-plus-circle class="w-6 h-6 hover:text-gray-700 transition duration-200 ease-in-out cursor-pointer"/>
+        <x-heroicon-s-plus-circle
+            class="w-6 h-6 hover:text-gray-700 transition duration-200 ease-in-out cursor-pointer"/>
     </x-slot>
     <x-slot name="content">
         <form class="px-6 py-4" method="POST" action="{{ route('cart.add', $product) }}">
             @csrf
             @if($product->options()->count() > 0)
-            <div>
-                <p class="font-bold mb-2">Options</p>
+                <div>
+                    <p class="font-bold mb-2">Options</p>
 
-                @foreach($product->options->groupBy(fn ($option) => $option->category->name) as $optionCategory => $options)
-                    <p class="mb-2">{{ $optionCategory }}</p>
+                    <div class="flex flex-col gap-4">
+                        @foreach($product->options->groupBy(fn ($option) => $option->category->name) as $optionCategory => $options)
+                            <div>
+                                <p class="mb-2">{{ $optionCategory }}</p>
 
-                    <div class="flex flex-col gap-2">
-                        @foreach($options as $option)
-                            <x-label class="flex gap-1 items-center">
-                                <input type="checkbox" value="{{ $option->id }}" name="options[]" />
-                                <span>{{ $option->name }}</span>
-                                <span>{{ $option->price }} DH</span>
-                            </x-label>
+                                <div class="flex flex-col gap-2">
+                                    @foreach($options as $option)
+                                        <x-label class="flex gap-2 items-center justify-between">
+                                            <div class="flex items-center">
+                                                <input type="checkbox" value="{{ $option->id }}" name="options[]"/>
+                                                <span class="ml-2">{{ $option->name }}</span>
+                                            </div>
+                                            <span>{{ $option->price }} DH</span>
+                                        </x-label>
+                                    @endforeach
+                                </div>
+                            </div>
                         @endforeach
                     </div>
-                @endforeach
-            </div>
+                </div>
             @endif
 
             <div class="my-6">
@@ -32,7 +39,7 @@
                     <div class="text-base font-bold mb-2">
                         Quantité
                     </div>
-                    <x-input type="number" name="quantity" class="block w-full"/>
+                    <x-input type="number" name="quantity" class="block w-full" value="1"/>
                 </x-label>
             </div>
 
