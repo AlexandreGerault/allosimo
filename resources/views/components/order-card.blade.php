4c758bc88a31c4fa5ae1675e6d1bac359570f6b1
Alexandre Gérault <alexandre@hexium.io>
2021-06-08 02:34:49 +0200

    feat(orders): orders content

--- resources/views/components/order-card.blade.php
+++ resources/views/components/order-card.blade.php
@@ -20,6 +20,43 @@
 
         <div class="my-2">
             <p class="text-sm text-gray-50">{{ $order->user->address }}, {{ $order->user->town }}</p>
+
+            <div class="flex flex-col gap-6 mt-6">
+                @foreach($order->lines as $line)
+                    <div class="bg-gray-50 rounded overflow-hidden px-6 py-4">
+                        <div class="flex gap-4">
+                            <div>
+                                {{ $line->quantity }}
+                            </div>
+                            <div class="flex-grow">
+                                <div class="flex gap-4 justify-between">
+                                    <div>
+                                        {{ $line->product->name }}
+                                    </div>
+                                    <div>
+                                        {{ $line->product->price }} DH
+                                    </div>
+                                </div>
+                                <div class="pl-6">
+                                    @foreach($line->options as $option)
+                                        <div class="flex justify-between">
+                                            <div>{{ $option->name }}</div>
+                                            <div>{{ $option->price }} DH</div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="flex justify-end mt-2 font-semibold">
+                            Total : {{ $line->price }} DH
+                        </div>
+                    </div>
+                @endforeach
+                <div class="flex justify-end mx-4 text-xl font-bold mt-6 text-white">
+                    Total : {{ $order->price }} DH
+                </div>
+            </div>
         </div>
     </div>
     </footer>
