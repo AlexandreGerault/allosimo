3de4522d55b92a05852186fe0335627e3eb5f917
Alexandre Gérault <alexandre@hexium.io>
2021-06-08 02:22:43 +0200

    feat add js

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -55,7 +55,7 @@
                                                             </figure>
                                                             <div class="flex justify-between">
                                                                 <h4>{{ $product->name }}</h4>
-                                                                <em class="flex gap-2">
+                                                                <em class="flex gap-1">
                                                                     <span>{{ $product->price }} DH</span>
                                                                     <x-product-option-card :product="$product"/>
                                                                 </em>
